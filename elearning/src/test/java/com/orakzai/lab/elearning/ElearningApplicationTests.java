809fe9c8607d290b8ff79c53dd1df9c7c615a1ed
Shah <khansss1915@gmail.com>
2020-07-17 14:44:09 +0400

    initial commit

---
+++ elearning/src/test/java/com/orakzai/lab/elearning/ElearningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.orakzai.lab.elearning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElearningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
